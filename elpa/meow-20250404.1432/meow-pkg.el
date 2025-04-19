f85c4a65441a7dc4fb40f2091574a4feb69e9c76
titanium-doll <cudeprogradealien@protonmail.com>
2025-04-19 22:43:50 +0200

    Removed elpa

--- elpa/meow-20250404.1432/meow-pkg.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "meow" "20250404.1432"
-  "Yet Another modal editing."
-  '((emacs "27.1"))
-  :url "https://www.github.com/DogLooksGood/meow"
-  :commit "e5817f3b5ef82cbcf8a729af205191dc6b142282"
-  :revdesc "e5817f3b5ef8"
-  :keywords '("convenience" "modal-editing"))
