3df5e721c53af234739f81805af0090c53a5bfe5
titanium-doll <cudeprogradealien@protonmail.com>
2025-05-17 14:57:09 +0200

    Update custom

--- custom.el
+++ custom.el
@@ -4,7 +4,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(beacon diminish doom-themes haskell-mode highlight-defined
+	    markdown-mode meow org-superstar paren-face solaire-mode
+	    tree-sitter-langs vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
