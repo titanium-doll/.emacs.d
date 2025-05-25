202e1a3970bf0491dbfbb3814a5e8f0c830c17c7
titanium-doll <cudeprogradealien@protonmail.com>
2025-05-25 22:22:44 +0200

    Update config

--- config.el
+++ config.el
@@ -1,6 +1,6 @@
 (if (daemonp)
     (progn
-	(setq default-directory "/home/lopsi/")))
+      (setq default-directory "/home/lopsi/")))
 
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file 'noerror)
@@ -16,8 +16,8 @@
 
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			   ("org" . "https://orgmode.org/elpa/")
-			   ("elpa" . "https://elpa.gnu.org/packages/")))
+			 ("org"   . "https://orgmode.org/elpa/")
+			 ("elpa"  . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -28,37 +28,71 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package diminish)
+(use-package dashboard
+  :after (nerd-icons)
+  :custom
+  (dashboard-banner-logo-title "this lee's emacs config ^-^")
+  (dashboard-startup-banner 'logo)
+
+  (dashboard-footer-messages '("^w^"
+			       ">~<"
+			       ".,."
+			       "<.<"
+			       "~v~"
+			       "~-~"
+			       "-.-"
+			       "zZz"
+			       "emacs is sooo"))
+  
+  (dashboard-center-content            t)
+  (dashboard-vertically-center-content t)
+  
+  (dashboard-item-names '(("Recent Files:" . "recent")
+			  ("Bookmarks:"    . "bookmarks")))
+  
+  (dashboard-navigation-cycle t)
+  (dashboard-item-shortcuts '((recents   . "t")
+                              (bookmarks . "o")))
+  (dashboard-items          '((recents   . 5)
+			      (bookmarks . 5)))
+  
+  (dashboard-icon-type 'nerd-icons)
+  (dashboard-display-icons-p   t)
+  (dashboard-set-heading-icons t)
+  (dashboard-set-file-icons    t)
+  
+  :hook (dashboard-mode . (lambda () (setq mode-line-format nil)))
+  :config (dashboard-setup-startup-hook))
 
 (column-number-mode)
 
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)
 (dolist (mode '(org-mode-hook
-		  term-mode-hook
-		  eshell-mode-hook
-		  dired-mode-hook
-		  eww-mode-hook
-		  package-menu-mode-hook
-		  help-mode-hook
-		  completion-list-mode-hook))
+		term-mode-hook
+		eshell-mode-hook
+		dired-mode-hook
+		eww-mode-hook
+		package-menu-mode-hook
+		help-mode-hook
+		completion-list-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-(dolist (set-keys '(("C-c bm" . buffer-menu)
-		    ("C-c bn" . next-buffer)
-		    ("C-c bp" . previous-buffer)
-		    ("C-c d" . dired-jump)
-		    ("C-c q" . kill-buffer-and-window)
-		    ("C-c sc" . sort-columns)
+(dolist (set-keys '(("C-c bm"  . buffer-menu)
+		    ("C-c bn"  . next-buffer)
+		    ("C-c bp"  . previous-buffer)
+		    ("C-c d"   . dired-jump)
+		    ("C-c q"   . kill-buffer-and-window)
+		    ("C-c sc"  . sort-columns)
 		    ("C-c sf0" . sort-fields)
 		    ("C-c sf1" . sort-regexp-fields)
 		    ("C-c sf2" . sort-numeric-fields)
-		    ("C-c sl" . sort-lines)
+		    ("C-c sl"  . sort-lines)
 		    ("C-c sp0" . sort-paragraphs)
 		    ("C-c sp1" . sort-pages)
- 		    ("C-c l" . eval-buffer)
- 		    ("C-c n" . display-line-numbers-mode)
- 		    ("C-c x" . meow-M-x)))
+ 		    ("C-c l"   . eval-buffer)
+ 		    ("C-c n"   . display-line-numbers-mode)
+ 		    ("C-c x"   . meow-M-x)))
   (define-key global-map (kbd (car set-keys)) (cdr set-keys)))
 
 (use-package meow
@@ -194,87 +228,90 @@
 
   (meow-global-mode 1))
 
-(set-face-attribute 'default nil :font "Maple Mono NF" :height 150)
-(set-face-attribute 'fixed-pitch nil :font "Maple Mono NF" :height 150 :weight 'bold)
+(set-face-attribute 'default           nil :font "Maple Mono NF" :height 150)
+(set-face-attribute 'fixed-pitch       nil :font "Maple Mono NF" :height 150 :weight 'bold)
 (set-face-attribute 'fixed-pitch-serif nil :font "Maple Mono NF" :height 150 :weight 'bold)
-(set-face-attribute 'variable-pitch nil :font "Maple Mono NF" :height 150)
+(set-face-attribute 'variable-pitch    nil :font "Maple Mono NF" :height 150)
 
 (use-package haskell-mode)
 
 (use-package lsp-mode
-  :diminish
-  :hook (haskell-mode . lsp))
+  :hook (haskell-mode . lsp)
+  :commands lsp)
 
 (use-package vertico
-  :custom
-  (vertico-cycle t)
-  :init
-  (vertico-mode))
+  :custom (vertico-cycle t)
+  :init (vertico-mode))
+
+(use-package marginalia
+  :bind (:map minibuffer-local-map
+	      ("M-a" . marginalia-cycle))
+  :init (marginalia-mode))
 
 (use-package company
-  :diminish
-  :hook (after-init-hook . global-company-mode))
+  :init (global-company-mode))
 
 (display-time-mode 1)
+
+(use-package mood-line
+  :config (mood-line-mode))
 
 (defun set-reading-margins ()
   "Set sane reading margins in current buffer."
   (interactive)
-  (setq left-margin-width 20)
+  (setq left-margin-width  20)
   (setq right-margin-width 20))
+
+(use-package nerd-icons)
 
 (use-package doom-themes
   :custom
-  (doom-themes-enable-bold t)
+  (doom-themes-enable-bold    t)
   (doom-themes-enable-italics t)
+
   :custom-face
   (font-lock-keyword-face ((nil (:slant italic))))
-  (org-document-title ((nil (:weight normal :height 1.0))))
-  (org-level-1 ((nil (:weight bold :height 1.20))))
-  (org-level-2 ((nil (:weight bold :height 1.15))))
-  (org-level-3 ((nil (:weight bold :height 1.10))))
-  (org-level-4 ((nil (:weight bold :height 1.05))))
+  (org-document-title     ((nil (:weight normal :height 1.0))))
+  (org-level-1            ((nil (:weight bold   :height 1.20))))
+  (org-level-2            ((nil (:weight bold   :height 1.15))))
+  (org-level-3            ((nil (:weight bold   :height 1.10))))
+  (org-level-4            ((nil (:weight bold   :height 1.05))))
+  
   :config
   (load-theme 'doom-outrun-electric t)
-
+  
   (doom-themes-visual-bell-config)
   (doom-themes-org-config))
 
 (use-package solaire-mode
-  :diminish
-  :config
-  (solaire-global-mode 1))
+  :config (solaire-global-mode 1))
 
 (use-package beacon
-  :config
-  (beacon-mode 1))
+  :config (beacon-mode 1))
 
 (use-package tree-sitter
-  :diminish
   :hook (sh-mode . tree-sitter-hl-mode)
-  :config
-  (global-tree-sitter-mode 1))
+  :config (global-tree-sitter-mode 1))
 
 (use-package tree-sitter-langs)
 
 (use-package highlight-defined
   :custom
-  (highlight-defined-function-name-face ((nil (:inherit tree-sitter-hl-face:function))))
+  (highlight-defined-function-name-face         ((nil (:inherit tree-sitter-hl-face:function))))
   (highlight-defined-builtin-function-name-face ((nil (:inherit tree-sitter-hl-face:function.builtin))))
-  (highlight-defined-macro-name-face ((nil (:inherit tree-sitter-hl-face:function.macro))))
-  (highlight-defined-variable-name-face ((nil (:inherit tree-sitter-hl-face:variable))))
+  (highlight-defined-macro-name-face            ((nil (:inherit tree-sitter-hl-face:function.macro))))
+  (highlight-defined-variable-name-face         ((nil (:inherit tree-sitter-hl-face:variable))))
+  
   :hook
   (emacs-lisp-mode . highlight-defined-mode)
-  (org-mode . highlight-defined-mode))
+  (org-mode        . highlight-defined-mode))
 
 (use-package paren-face
-  :custom-face
-  (parenthesis ((t (:inherit 'font-lock-comment-face))))
-  :config
-  (global-paren-face-mode 1))
+  :custom-face (parenthesis ((t (:inherit 'font-lock-comment-face))))
+  :config (global-paren-face-mode 1))
 
 (setq initial-major-mode 'org-mode
-	initial-scratch-message nil)
+      initial-scratch-message nil)
 
 (defun load-current-org-file ()
   "Tangle and load the current Org file."
@@ -290,8 +327,21 @@
   :custom
   (org-hide-emphasis-markers t)
   (org-superstar-leading-bullet ?\s)
+  (org-indent-mode-turns-on-hiding-stars nil)
+  (org-superstar-headline-bullets-list '("❀" "❖" "✦" "✧"))
+  (org-superstar-prettify-item-bullets t)
+  (org-superstar-item-bullet-alist '((?* . ?•)
+				     (?+ . ?→)
+				     (?- . ?•)))
   :hook org-mode)
 
-(add-hook 'org-mode-hook 'set-reading-margins)
+(setq org-src-preserve-indentation nil)
+(setq org-edit-src-content-indentation 0)
+
+(defun org-mode-load-functions ()
+  (set-reading-margins)
+  (org-indent-mode))
+
+(add-hook 'org-mode-hook 'org-mode-load-functions)
 
 (require 'org-tempo)
